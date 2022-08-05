0dbe6d934fcfd06d615801b70c1ec70de77e9810
Anthony Attwood <anthony.attwood@purple.telstra.com>
2022-08-05 08:44:50 +0800

    let's see where we are

--- build/Build.cs
+++ build/Build.cs
@@ -19,23 +19,23 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 [GitHubActions(
     "test",
     GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = false,
     // On = new[] { GitHubActionsTrigger.Push },
     InvokedTargets = new[] { nameof(Test) },
-    OnPushBranchesIgnore = new[] { "main", "origin/main" },
-    AutoGenerate = false)]
+    OnPushBranchesIgnore = new[] { "main", "origin/main" })]
 [GitHubActions(
     "pull-request",
     GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = false,
     On = new[] { GitHubActionsTrigger.PullRequest },
-    InvokedTargets = new[] { nameof(Cover), nameof(IntegrationTest) },
-    AutoGenerate = false)]
+    InvokedTargets = new[] { nameof(Cover), nameof(IntegrationTest) })]
 [GitHubActions(
     "release",
     GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = false,
     // On = new[] { GitHubActionsTrigger.Push },
     InvokedTargets = new[] { nameof(Cover), nameof(DockerPush) },
-    OnPushBranches = new[] { "main", "origin/main" },
-    AutoGenerate = false)]
+    OnPushBranches = new[] { "main", "origin/main" })]
 class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.Compile);
